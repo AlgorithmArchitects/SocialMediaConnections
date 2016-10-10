2fde3334c396200fce8d09d32436640ceeeff826
Carter Thayer <carterwthayer@gmail.com>
2016-10-09 20:37:25 -0500

    Allow for providing a screen name

--- src/app/graph/graph.js
+++ src/app/graph/graph.js
@@ -5,10 +5,16 @@ import './graph.css';
 class Graph extends Component {
   constructor(props, context){
     super(props, context);
-    this.state = {twitterPeople: []};
+    this.state = {twitterPeople: [], twitter_screen_name: null};
+    this.changeTwitterScreenName = this.changeTwitterScreenName.bind(this);
+    this.submitTwitter = this.submitTwitter.bind(this);
   }
-  componentDidMount(){
-    fetch('/twitter?screen_name=carterthayer')
+  changeTwitterScreenName(e){
+    this.state.twitter_screen_name = e.target.value;
+  }
+  submitTwitter(e){
+    e.preventDefault();
+    fetch('/twitter?screen_name=' + this.state.twitter_screen_name)
         .then(response => response.json())
         .then(data => this.setState({ twitterPeople: data }))
         .catch(err => console.error(err));
@@ -22,6 +28,10 @@ class Graph extends Component {
     return (
       <div className="graph">
         <div className="twitter">
+            <form onSubmit={this.submitTwitter}>
+                <input name="twitter_screen_name" ref="twitter_screen_name" onChange={this.changeTwitterScreenName}/>
+                <input type="submit" value="Submit" />
+            </form>
             {twitterPeople}
         </div>
         <div className="facebook">
